02043dc80aba0c2c120cf43f8e1aa835a4b13ac3
luka9kw <luka9kw@hotmail.com>
2024-04-23 22:14:26 -0300

    commit inicial

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/* console.log(`string, break line, \n\
+i dont know how this works, but works! \n\
+one more line \n\
+just one more \n\
+okay, i'm finished \n\ `);
+
+const massMark = 78;
+const heighMark = 1.69;
+const massJohn = 92;
+const heighJohn = 1.95;
+
+const BMIMark = massMark / heighMark ** 2;
+const BMIJohn = massJohn / (massJohn * massJohn);
+
+
+if (BMIMark > BMIJohn) {
+    console.log(`Mark's BMI (${BMIMark}) is higher than John's(${BMIJohn})!`)
+} else {
+    console.log(`John's BMI (${BMIJohn}) is higher than Marks's(${BMIMark})!`)
+} */
+
+// Ternary Operator
+
+/* const age = 15;
+const drink = age >= 18 ? 'wine' : 'water';
+console.log(drink) */
+
