2fa7d87e0d76957cb9a57898ad09ddabbb3ef731
Luka9kw <luka9kw@hotmail.com>
2024-05-03 10:35:13 -0300

    te

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,1 +1,15 @@
 'use scrict';
+
+
+const cutFruitPieces = fruit => fruit * 4;
+
+const fruitProcessor = (apples, oranges) => {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces oranges`;
+
+    return juice;
+};
+
+console.log(fruitProcessor(3, 8))
