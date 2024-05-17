00b7d432ea85062c1bd1805acbc16a86873ab423
Luka9kw <luka9kw@hotmail.com>
2024-05-17 10:12:51 -0300

    guess my number first comit

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+// Criar o sistema que escolhe um numero aleatorio entre 1 e 20 ao clicar no botao CHECK
+
+const btnCheck = document.querySelector('.check');
+btnCheck.addEventListener('click', function () {
+  console.log('hello world');
+});
+
+const randomNumber = function () {};
+
+// Criar o verificador de vitória  caso os numeros sejam iguais
+
+// Criar o score: O sistema começa com 20 pontos e retira 1 ponto a cada erro
+
+// Criar o highscore: O sistema registra qual foi sua pontuação mais alta (menos erros).
+
+// Criar o high ou low: Sistema que indica se o seo valor foi muito acima ou abaixo do valor aleatório da partida.
